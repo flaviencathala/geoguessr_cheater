7c1a22e5b77178c8d0c8a9aceb6baaf1d11c79b2
Flavien Cathala <flavien.cathala@gmail.com>
2017-03-18 15:23:30 +0100

    display map with marker

--- cheater.js
+++ cheater.js
@@ -1,5 +1,12 @@
 var GMapsUrl = 'https://maps.googleapis.com/maps/api/js/GeoPhotoService.SingleImageSearch';
 
+function executeCode(scriptContent) {
+    var script = document.createElement('script');
+    script.id = 'tmpScript';
+    script.appendChild(document.createTextNode(scriptContent));
+    (document.body || document.head || document.documentElement).appendChild(script);
+}
+
 function isScript(event)
 {
   if (event.path.length && event.path[0].type === 'text/javascript')
@@ -21,9 +28,13 @@ document.addEventListener("load", function(event)
             var data = JSON.parse(realData);
             var cd1 = data[1][5][0][1][0][2];
             var cd2 = data[1][5][0][1][0][3];
-            chrome.runtime.sendMessage({cd1: cd1, cd2: cd2}, function(response) {
+            executeCode(`
+                var CMap = new google.maps.Map($('.game-usps')[0], {zoom: 2,center: {lat: ` + cd1 + `, lng: ` + cd2 + `}});
+                new google.maps.Marker({position: {lat:` + cd1 + `, lng: ` + cd2 + `}, map: CMap});
+            `);
+            /*chrome.runtime.sendMessage({cd1: cd1, cd2: cd2}, function(response) {
                 $('.account-status__sign-out').html(response.data.results[0].formatted_address);
-            });
+            });*/
         });
     }
   }
